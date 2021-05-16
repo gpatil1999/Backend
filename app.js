86166c1b6dc4ae04a30432ab553d930e6f3b4eb5
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2021-05-16 14:28:33 +0530

    Resolve CORS issue

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var FileStore=require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
+var cors = require('./routes/cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -37,15 +38,17 @@ const Leaders=require('./models/leaders');
 const Favourite=require('./models/favourite');
 
 const url = config.mongoUrl;
-const connect = mongoose.connect(url);
+// const connect = mongoose.connect(url);
 
-connect.then((db) => {
-    console.log("Connected correctly to server");
-}, (err) => { console.log(err); });
+// connect.then((db) => {
+//     console.log("Connected correctly to server");
+// }, (err) => { console.log(err); });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(cors.corsWithOptions);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -82,5 +85,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;

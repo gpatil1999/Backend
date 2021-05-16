ef17954f6dbe835ddb43e262081b36da8fe12009
gpatil1999 <67685276+gpatil1999@users.noreply.github.com>
2021-05-16 10:25:45 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/confusion',
+    'facebook':{
+        clientId: '216556199487273',
+        clientSecret:'01fcb940e956af25a45853b568f35dc0'
+    }
+}
